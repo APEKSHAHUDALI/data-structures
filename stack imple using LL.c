052e5a7aa36471b96f382b64a9921aec8a45b9db
APEKSHAHUDALI <apekshasanjay.ad24@bmsce.ac.in>
2026-01-06 15:24:22 +0530

    Add files via upload

---
+++ stack imple using LL.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+struct node *top = NULL;
+
+
+void push() {
+    struct node *newnode;
+    newnode = (struct node*)malloc(sizeof(struct node));
+
+    printf("Enter value to push: ");
+    scanf("%d", &newnode->data);
+
+    newnode->next = top;
+    top = newnode;
+
+    printf("Element pushed successfully\n");
+}
+
+
+void pop() {
+    struct node *temp;
+
+    if (top == NULL) {
+        printf("Stack is empty\n");
+        return;
+    }
+
+    temp = top;
+    printf("Popped element: %d\n", temp->data);
+    top = top->next;
+    free(temp);
+}
+
+
+void display() {
+    struct node *temp;
+
+    if (top == NULL) {
+        printf("Stack is empty\n");
+        return;
+    }
+
+    temp = top;
+    printf("Stack elements:\n");
+    while (temp != NULL) {
+        printf("%d\n", temp->data);
+        temp = temp->next;
+    }
+}
+
+
+int main() {
+    int choice;
+
+    while (1) {
+        printf("\n--- Stack Menu ---\n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+        case 1:
+            push();
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(0);
+        default:
+            printf("Invalid choice\n");
+        }
+    }
+    return 0;
+}
